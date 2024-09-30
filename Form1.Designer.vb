720c8cbfbd4c9e3d55190021affdca4771ab849c
GauravChauhan-07 <gauravchauhan7797@gmail.com>
2024-09-30 19:40:41 +0530

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,219 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        ListView1 = New ListView()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        Pro_Id = New TextBox()
+        Pro_Name = New TextBox()
+        Pro_Mrp = New TextBox()
+        Qty = New TextBox()
+        Gst_Rate = New TextBox()
+        Dis_Id = New TextBox()
+        Pro_Price = New TextBox()
+        Sub_CateId = New TextBox()
+        Button6 = New Button()
+        SuspendLayout()
+        ' 
+        ' ListView1
+        ' 
+        ListView1.Location = New Point(32, 15)
+        ListView1.Margin = New Padding(4, 4, 4, 4)
+        ListView1.Name = "ListView1"
+        ListView1.Size = New Size(1164, 369)
+        ListView1.TabIndex = 0
+        ListView1.UseCompatibleStateImageBehavior = False
+        ListView1.View = View.Details
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(1008, 419)
+        Button1.Margin = New Padding(4, 4, 4, 4)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(98, 38)
+        Button1.TabIndex = 9
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(1134, 419)
+        Button2.Margin = New Padding(4, 4, 4, 4)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(98, 38)
+        Button2.TabIndex = 10
+        Button2.Text = "Button2"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(1262, 419)
+        Button3.Margin = New Padding(4, 4, 4, 4)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(98, 38)
+        Button3.TabIndex = 11
+        Button3.Text = "Button3"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(1398, 419)
+        Button4.Margin = New Padding(4, 4, 4, 4)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(98, 38)
+        Button4.TabIndex = 12
+        Button4.Text = "Button4"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Button5
+        ' 
+        Button5.Location = New Point(1526, 419)
+        Button5.Margin = New Padding(4, 4, 4, 4)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(98, 38)
+        Button5.TabIndex = 13
+        Button5.Text = "Button5"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' Pro_Id
+        ' 
+        Pro_Id.Location = New Point(1596, 15)
+        Pro_Id.Margin = New Padding(4, 4, 4, 4)
+        Pro_Id.Name = "Pro_Id"
+        Pro_Id.Size = New Size(155, 31)
+        Pro_Id.TabIndex = 14
+        ' 
+        ' Pro_Name
+        ' 
+        Pro_Name.Location = New Point(1596, 56)
+        Pro_Name.Margin = New Padding(4, 4, 4, 4)
+        Pro_Name.Name = "Pro_Name"
+        Pro_Name.Size = New Size(155, 31)
+        Pro_Name.TabIndex = 15
+        ' 
+        ' Pro_Mrp
+        ' 
+        Pro_Mrp.Location = New Point(1596, 98)
+        Pro_Mrp.Margin = New Padding(4, 4, 4, 4)
+        Pro_Mrp.Name = "Pro_Mrp"
+        Pro_Mrp.Size = New Size(155, 31)
+        Pro_Mrp.TabIndex = 16
+        ' 
+        ' Qty
+        ' 
+        Qty.Location = New Point(1596, 139)
+        Qty.Margin = New Padding(4, 4, 4, 4)
+        Qty.Name = "Qty"
+        Qty.Size = New Size(155, 31)
+        Qty.TabIndex = 17
+        ' 
+        ' Gst_Rate
+        ' 
+        Gst_Rate.Location = New Point(1596, 180)
+        Gst_Rate.Margin = New Padding(4, 4, 4, 4)
+        Gst_Rate.Name = "Gst_Rate"
+        Gst_Rate.Size = New Size(155, 31)
+        Gst_Rate.TabIndex = 18
+        ' 
+        ' Dis_Id
+        ' 
+        Dis_Id.Location = New Point(1596, 221)
+        Dis_Id.Margin = New Padding(4, 4, 4, 4)
+        Dis_Id.Name = "Dis_Id"
+        Dis_Id.Size = New Size(155, 31)
+        Dis_Id.TabIndex = 19
+        ' 
+        ' Pro_Price
+        ' 
+        Pro_Price.Location = New Point(1596, 262)
+        Pro_Price.Margin = New Padding(4, 4, 4, 4)
+        Pro_Price.Name = "Pro_Price"
+        Pro_Price.Size = New Size(155, 31)
+        Pro_Price.TabIndex = 20
+        ' 
+        ' Sub_CateId
+        ' 
+        Sub_CateId.Location = New Point(1596, 304)
+        Sub_CateId.Margin = New Padding(4, 4, 4, 4)
+        Sub_CateId.Name = "Sub_CateId"
+        Sub_CateId.Size = New Size(155, 31)
+        Sub_CateId.TabIndex = 21
+        ' 
+        ' Button6
+        ' 
+        Button6.Location = New Point(395, 419)
+        Button6.Margin = New Padding(4, 4, 4, 4)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(154, 38)
+        Button6.TabIndex = 22
+        Button6.Text = "Button6"
+        Button6.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1768, 471)
+        Controls.Add(Button6)
+        Controls.Add(Sub_CateId)
+        Controls.Add(Pro_Price)
+        Controls.Add(Dis_Id)
+        Controls.Add(Gst_Rate)
+        Controls.Add(Qty)
+        Controls.Add(Pro_Mrp)
+        Controls.Add(Pro_Name)
+        Controls.Add(Pro_Id)
+        Controls.Add(Button5)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(ListView1)
+        Margin = New Padding(4, 4, 4, 4)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents ListView1 As ListView
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Pro_Id As TextBox
+    Friend WithEvents Pro_Name As TextBox
+    Friend WithEvents Pro_Mrp As TextBox
+    Friend WithEvents Qty As TextBox
+    Friend WithEvents Gst_Rate As TextBox
+    Friend WithEvents Dis_Id As TextBox
+    Friend WithEvents Pro_Price As TextBox
+    Friend WithEvents Sub_CateId As TextBox
+    Friend WithEvents Button6 As Button
+
+End Class
